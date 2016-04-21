3b5479ba012dd3d9dbf306c2a2b53f19da85cdfd
aqeel13932 <samer226047@gmail.com>
2016-04-21 19:08:45 +0300

    HW10 updates

--- HW10/Q5.R
+++ HW10/Q5.R
@@ -1,12 +1,28 @@
 rm(list=ls())
 setwd('/home/aqeel/Study/DM/HW10/')
 train <- read.csv('train.csv',header = TRUE)
-module<-lm(data = train,target~.)
-summary(train)
+train$sumusage<- rowSums(train[,seq(12,16,1)])
+cor(train)
+trainmixed<-train[sample(nrow(train)),]
+trainset<-trainmixed[seq(0,floor(0.8*nrow(trainmixed)),1),]
+testset<-trainmixed[seq(floor(0.8*nrow(trainmixed))+1,nrow(trainmixed),1),]
+
+module<-lm(data = trainset,target~connections+foreign_conns+foreign_conns2+ccf+poly(conns_new,4)+poly(time,2)+
+             poly(ccf_old,2)+account_age+poly(agegroup,3)+browserID+poly(days,4)+poly(usage1,2)+
+             usage2+poly(usage3,4)+poly(usage4,4)+poly(usage5,2)+sumusage)
+sqrt(mean((predict(module,testset)- testset$target)^2))
+summary(module)
 test<-read.csv('test.csv',header = TRUE)
 head(test[,c(1,2)])
 result<-predict(module,test)
 output<-cbind(result)
 colnames(output)<-c('ID','target')
 
-write.csv(output,file='submit 001')
+write.csv(output,file='submit 003')
+sort(table(train$target))
+full<- train[train$target==10,]
+head(full)
+max(full$account_age)
+empty<-train[train$target==0,]
+View(empty)
+max(train$account_age)
